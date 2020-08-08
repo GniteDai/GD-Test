8cb2f0916945d730b6b88247f274b1d665a709e5
Gnite <gnitekk@gmail.com>
2020-08-09 03:27:09 +0800

    build wall => For Test

--- components/algorithm/A-star-search.vue
+++ components/algorithm/A-star-search.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <div class="test-outer" v-for="arrayOuter of arrayTest" :key="arrayOuter.index">
-      <div class="test-inner" v-for="test of arrayOuter" :key="test" v-html="test"></div>
+    <div class="menu">
+      <div class="menu-item" @click="setBasic(true)">Build 全白</div>
+      <div @click="openRouteChange" :class="{ 'menu-item-Selected' : b_openRouteChange }">牆壁相反改變</div>
+      <div @click="openBuildWall" :class="{ 'menu-item-Selected' : b_openBuildWall }">建立牆壁</div>
+    </div>
+    <div class="test-outer" v-for="(row, row_index) in arrayTest" :key="row_index">
+      <div class="test-inner" ref="column" v-for="(column, column_index) in row" :key="column_index" v-html="column.info" @mouseover="changeRoute(row_index, column)"></div>
+      <!-- <div class="test-inner" ref="column" :data.prop="column" v-for="(column, column_index) in row" :key="column_index" v-html="column.info" @mouseover="changeRouteTest(column)"></div> -->
     </div>
   </div>
 </template>
@@ -11,25 +17,70 @@ export default {
   props: ['panel'],
   data(){
     return{
-      arrayTest: []
+      arrayTest: [],
+      b_openRouteChange: false,
+      b_openBuildWall: false
     }
   },
   mounted(){
-    this.setBasic();
-    console.log('setBasic after => ', this.arrayTest)
+    this.setBasic(false);
   },
   methods: {
-    setBasic(){
-      let newA = []
-      let newB = []
+    getRandomInt(){
+      let min = Math.ceil(0);
+      let max = Math.floor(2);
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+    setBasic(b_No_Wall){
+      // 直行橫列
+      // 行
+      let row = []
+      // 列
+      let column = []
+      let data = {}
       for(let i=0; i<=10; i++){
-        newB = []
+        column = []
         for(let j = 0; j<=10;j++){
-          newB.push(j);
+          data = {
+            x: i,
+            y: j,
+            wall: this.buildWall(b_No_Wall),
+            pass: false,
+            start: false,
+            end: false
+          }
+          data.info = data.wall ? 'X' : 'o';
+          column.push(data);
+          data = {}
         }
-        newA.push(newB)
+        row.push(column)
       }
-      this.arrayTest = newA;
+      this.arrayTest = row;
+    },
+    changeRoute(outer, inner){
+      if(this.b_openRouteChange){
+        inner.wall = !inner.wall;
+        inner.info = inner.wall ? 'X' : 'o';
+      }else if(this.b_openBuildWall){
+        inner.wall = true;
+        inner.info = 'X';
+      }
+    },
+    changeRouteTest(inner){
+      // Test => Get elements Data property
+      console.log('data => ', this.$refs.column[0].data)
+    },
+    openRouteChange(){
+      this.b_openRouteChange = !this.b_openRouteChange;
+      this.b_openBuildWall = false;
+    },
+    buildWall(b_No_Wall){
+      let wall = b_No_Wall ? false : this.getRandomInt() === 0 ? true : false
+      return wall
+    },
+    openBuildWall(){
+      this.b_openBuildWall = !this.b_openBuildWall;
+      this.b_openRouteChange = false;
     }
   }
 }
@@ -39,16 +90,39 @@ export default {
 </style>
 
 <style scoped>
+  .menu {
+    width: 100%;
+    height: auto;
+    color: greenyellow;
+    display: flex;
+    justify-content: space-around;
+    align-content: center;
+    margin-bottom: 3%;
+    cursor: pointer;
+  }
+  .menu-item:hover {
+    color: red;
+    background-color: white;
+  }
+  .menu-item-Selected {
+    color: red;
+    background-color: white;
+  }
   .test-outer {
     width: 100%;
     height: auto;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-content: center;
   }
   .test-inner {
     color: orange;
-    background-color: green;
     font-size: 1.67vw;
+    cursor: default;
+    border: 1px solid green;
+  }
+  .test-inner:hover {
+    background: white;
+    color: red;
   }
 </style>
